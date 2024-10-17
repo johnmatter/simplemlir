8853e129d591020c01d746647b1d6a3b090249c1
John Matter <jmatter4@gmail.com>
2024-10-17 16:34:48 -0400

    lower linalg.batch_maul to linalg.generic with linalg-generalize-named-ops

--- lower.py
+++ lower.py
@@ -5,7 +5,7 @@ import pdb
 
 def print_step_name(step_name):
     try:
-        subprocess.run(['figlet', step_name], check=True)
+        subprocess.run(['figlet', '-w 200', step_name], check=True)
     except FileNotFoundError:
         line = '-' * len(step_name)
         print(f"\n{line}\n{step_name}\n{line}\n")
@@ -22,7 +22,6 @@ def run_command(command, args, working_dir, input_mlir, index, step_name):
     full_command = f"{command} {' '.join(args)} {input_mlir} > {output_file} 2> {error_file}"
     
     # Execute the command
-    # breakpoint()
     subprocess.run(full_command, shell=True, check=True)
     
     return output_file  # Return the output file path for the next pass
@@ -31,6 +30,9 @@ def get_absolute_path(path):
     return path if os.path.isabs(path) else os.path.abspath(path)
 
 def main():
+    # Run clean.sh
+    subprocess.run('rm -f 0*err 0*mlir', shell=True, check=True)
+
     # Load the JSON file
     with open('passes.json', 'r') as f:
         passes = json.load(f)['passes']
@@ -45,8 +47,26 @@ def main():
 
     # Run each pass
     for index, pass_info in enumerate(passes):
-        print_step_name(pass_info['name'])
-        input_mlir = run_command(pass_info['command'], pass_info['args'], working_dir, input_mlir, index+1, pass_info['name'])
+
+        # Print the step name with index prefix
+        print_step_name(f"( {index + 1} ) {pass_info['name']}")
+
+        # Run the pass
+        output_mlir = run_command(pass_info['command'], pass_info['args'], working_dir, input_mlir, index+1, pass_info['name'])
+
+        # Check if the output file differs from the input file
+        if input_mlir.endswith('.mlir'):
+            with open(input_mlir, 'r') as f:
+                input_content = f.read()
+            with open(output_mlir, 'r') as f:
+                output_content = f.read()
+            if input_content == output_content:
+                print(f"Output file {output_mlir} is identical to input file {input_mlir}")
+                break
+
+        # Update input_mlir for the next pass
+        input_mlir = output_mlir
+
 
 if __name__ == "__main__":
     main()
